590ee205d54469de0d54d771fa3b6ee22de3fdd4
vishal <vishal.meshram@viit.ac.in>
2023-04-29 09:20:57 +0530

    added all files to project

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# MNet-MergeNet-to-reduce-image-misclassification
-a proposed framework to reduce image misclassificationn
+# MNet: A Framework to Reduce Fruit Image Misclassification 
+## original artcile is avaialble at https://www.iieta.org/journals/isi/paper/10.18280/isi.260203
+
+This repositoy cosnists of 3 python files: 
+1) FC_with_inceptionv3_plainModel.py
+2) FC_with_inceptionv3_TL.py
+3) FC_with_inceptionv3_MNet.py
+
+## Fruit classification using pre-trained model:
+To classify the fruits "inceptionv3" pre-trained model is used. you can refer the Keras Applications (https://keras.io/api/applications/) and can try the code with differnt pre-trained models.
+
+## Dataset 
+Sample images are used from the original dataset in this project. 
+Datasets are published and available at:
+1) FRUITSGB: TOP INDIAN FRUITS WITH QUALITY (link: https://ieee-dataport.org/open-access/fruitsgb-top-indian-fruits-quality )
+    ( Cite as: Vishal Meshram, Koravat Thanomliang, Supawadee Ruangkan, Prawit Chumchu, Kailas Patil, July 8, 2020, "FruitsGB: Top Indian Fruits with quality ", IEEE Dataport, doi: https://dx.doi.org/10.21227/gzkn-f379.)
+2) FruitNet: Indian Fruits Dataset with quality (Good, Bad & Mixed quality) (link: https://data.mendeley.com/datasets/b6fftwbr2v/2)
+    (Cite as: PATIL, Kailas; MESHRAM, Vishal (2021), “FruitNet: Indian Fruits Dataset with quality (Good, Bad & Mixed quality)”, Mendeley Data, V2, doi: 10.17632/b6fftwbr2v.2)
+
+
+# Code Files (.py programs)
+### 1. FC_with_inceptionv3_plainModel
+In this project weights of the inceptionv3 model are used. 4 layers are added at the end: global average pooling layer,  dense layer, dropout layer, and 1 softmax layer.
+
+### 2. FC_with_inceptionv3_TL
+Fruit classification with "Trasnfer Learning" technique. where 20% of layers are freezed and model is retrained on the dataset.
+
+### 2. FC_with_inceptionv3_MNet
+Fruit classification with "Trasnfer Learning" using MNet framework. we created separated models i.e. one for fruti classification and one for quality. in the last phase output is merged. 
+
+
+### Hyperparameters tuning
+Hyperparameters in Machine learning are those parameters that are explicitly defined by the user to control the learning process. some examles are: 1) Learning rate for training a neural network 2) Train-test split ratio 3) Batch Size 4) Number of Epochs. As per the rquirement user can chage these parameters in the code.
+
+### Other related articles:
+1) FruitNet: Indian fruits image dataset with quality for machine learning applications (link: https://www.sciencedirect.com/science/article/pii/S2352340921009616)
+   (Cite as: Vishal Meshram, Kailas Patil, FruitNet: Indian fruits image dataset with quality for machine learning applications, Data in Brief, Volume 40, (2022), 107686, https://doi.org/10.1016/j.dib.2021.107686)
+2) Machine learning in agriculture domain: A state-of-art survey (link: https://www.sciencedirect.com/science/article/pii/S2667318521000106)
+   (Cite as: Vishal Meshram, Kailas Patil, Vidula Meshram, Dinesh Hanchate, S.D. Ramkteke, Machine learning in agriculture domain: A state-of-art survey, Artificial Intelligence in the Life Sciences, Volume 1, (2021), 100010, https://doi.org/10.1016/j.ailsci.2021.100010)
+
---
+++ code/FC_with_inceptionv3_TL.py
@@ -0,0 +1,170 @@
+import numpy as np
+import pandas as pd 
+from keras.preprocessing.image import ImageDataGenerator
+from keras_preprocessing.image import load_img
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+import os
+import tensorflow as tf
+
+from tensorflow.keras import layers,models
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.applications.inception_v3 import preprocess_input, decode_predictions
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from sklearn.metrics import classification_report, confusion_matrix
+
+import seaborn as sns
+from tensorflow.keras import optimizers
+
+#PHASE 1:- DataSet Preparation
+# create a array of classes names
+filenameAll = os.listdir("/data/AllPhotos") # dataset path.
+categoryAll = []
+for filename in filenameAll:
+  if("Apple" in filename):
+    categoryAll.append("Apple")
+  if("Banana" in filename):
+    categoryAll.append("Banana")
+  if("Guava" in filename):
+    categoryAll.append("Guava")
+  if("Lime" in filename):
+    categoryAll.append("Lime")
+  if("Orange" in filename):
+    categoryAll.append("Orange")
+  if("Pomegranate" in filename):
+    categoryAll.append("Pomegranate")
+
+classifyOnlyFruitdf = pd.DataFrame({
+    'filename': filenameAll,
+    'category': categoryAll
+})
+
+classifyOnlyFruitdf = classifyOnlyFruitdf.sort_values(by ='category', ascending = 1)
+
+classifyOnlyFruitdf['category'].value_counts().plot.bar()
+
+train_df, test_df = train_test_split(classifyOnlyFruitdf, test_size=0.20, random_state=42) #cosider 80:20 for training and testing
+train_df = train_df.reset_index(drop=True)
+test_df = test_df.reset_index(drop=True)
+
+total_train = train_df.shape[0]
+total_validate = test_df.shape[0]
+batch_size=30
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+#map the data frames and Images
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "/data/AllPhotos", 
+    x_col="filename",
+    y_col="category",
+    target_size=(299, 299),
+    class_mode="categorical",
+    batch_size=batch_size
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_generator = test_datagen.flow_from_dataframe(
+    test_df, 
+    "/data/AllPhotos", 
+    x_col='filename',
+    y_col='category',
+    target_size=(299, 299),
+    class_mode='categorical',
+    batch_size=batch_size,
+    shuffle=False
+)
+                                      
+
+#PHASE 2: Model architecture
+#select pretrained model as InceptionV3
+pretrainedModel = InceptionV3(input_shape=(299,299,3), weights="imagenet", include_top=False)
+last_output = pretrainedModel.output
+x = layers.GlobalAveragePooling2D()(last_output)
+x = layers.Dense(1024, activation='relu')(x)
+x = layers.Dropout(0.25)(x)
+x = layers.Dense(6, activation='softmax')(x)
+
+model = Model(pretrainedModel.input, x)
+
+#to print our model layes use model isntead of pretrainend 
+for i, layer in enumerate(pretrainedModel.layers):
+   print(i, layer.name)
+
+# Freez the first 62 layers and retrain the model again (62 means 20% of layers 310)
+for layer in model.layers[:62]:
+   layer.trainable = False
+
+for layer in model.layers[62:]:
+   layer.trainable = True        
+                                                        
+sgd = optimizers.SGD(lr=0.0001, momentum=0.9)
+model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+
+#Train the Model
+history = model.fit(train_generator,
+                              validation_data=test_generator,
+                              steps_per_epoch=total_train/batch_size,
+                              epochs=5,
+                              validation_steps=total_validate/batch_size,
+                              verbose=1)
+
+
+
+# Test the model 
+classes = ['Apple','Banana', 'Guava', 'Lime', 'Orange', 'Pomegranate']
+
+img_path = '/data/TestingImages/AppleGood21.jpg'
+img = image.load_img(img_path, target_size=(299, 299, 3))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+
+preds = model.predict([x])
+print(classes[np.argmax(preds)])
+print(preds)
+
+model.save("results/FCInceptionV3_TL.h5")
+
+#===============================================================
+#Follwoing code can be used to print confusion matrix
+#from sklearn.metrics import classification_report, confusion_matrix
+
+#Y_pred = model.predict_generator(test_generator, total_validate // batch_size+1)
+#y_pred = np.argmax(Y_pred, axis=1)
+#labels = [1,2,3,4,5,6]
+#print('Confusion Matrix')
+#cm = confusion_matrix(test_generator.classes,y_pred)
+#target_names=["Apple", "Banana", "Guava", "Lime", "Orange","Pomegranate"]
+
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+
+#ax = plt.subplot()
+#fig_dims = (10,7)
+#fig,ax = plt.subplots(figsize=fig_dims);
+
+#sns.heatmap(cm/np.sum(cm),fmt=".2%",annot=True,ax = ax,cmap="Blues");
+
+#labels, title and ticks
+#sns.set(font_scale=1) # for label size
+#ax.set_xlabel('Predicated Labels');ax.set_ylabel('True labels');
+#ax.set_title('Confusion Matrix');
+#ax.xaxis.set_ticklabels(target_names); ax.yaxis.set_ticklabels(target_names);
